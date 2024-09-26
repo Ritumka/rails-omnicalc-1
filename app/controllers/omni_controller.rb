47ec51aeb3fb02dc2bccb58e490bfa5f82365e5e
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-09-26 03:52:58 +0000

    a half

---
+++ app/controllers/omni_controller.rb
@@ -0,0 +1,66 @@
+class OmniController < ApplicationController
+  def nsquare
+    render({ :template => "new/square_new" })
+  end
+
+  def rsquare
+    @the_num = params.fetch("user_number").to_f
+    @the_result = @the_num ** 2
+
+    render({ :template => "results/square_result" })
+  end
+
+
+  def nsquare_root
+    render({ :template => "new/square_root_new" })
+  end
+
+  def rsquare_root
+    @the_num = params.fetch("user_number").to_f
+    @the_result = Math.sqrt(@the_num)
+
+    render({ :template => "results/square_root_result" })
+  end
+
+end
+
+
+
+
+# get("/payment/new") do
+#   erb(:payment_new)
+
+# end
+
+# get("/payment/results") do
+#   @apr = params.fetch("user_apr").to_f
+#   @years = params.fetch("user_years").to_f
+#   @pv = params.fetch("user_pv").to_f
+
+#   r = (@apr / 100) / 12
+#   n = @years * 12
+
+#   numerator = r * @pv
+#   denominator = 1 - (1 + r) ** -n
+
+#   @payment = numerator / denominator
+
+#   erb(:payment_results)
+
+# end
+
+# get("/random/new") do
+
+#   erb(:random_new)
+
+# end
+
+# get("/random/results") do
+#   @min = params.fetch("user_min").to_f
+#   @max = params.fetch("user_max").to_f
+
+#   @random_number =rand(@min..@max)
+
+#   erb(:random_results)
+
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get("/square/new", { :controller => "omni", :action => "nsquare" })
+  get("/square/results", { :controller => "omni", :action => "rsquare" })
+
+  get("/square_root/new", { :controller => "omni", :action => "nsquare_root" })
+  get("/square_root/results", { :controller => "omni", :action => "rsquare_root" })
+
+
+  # get("/payment/new", { :controller => "omni", :action => "payment" })
+
+  # get("/random/new", { :controller => "omni", :action => "random" })
+  
+
+  get("/", { :controller => "omni", :action => "nsquare" })
   
 end
